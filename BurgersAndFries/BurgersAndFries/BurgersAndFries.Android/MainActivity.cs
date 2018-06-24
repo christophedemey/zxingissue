4160b241a17dcd2337a3c7857b73b68f574044bb
christophedm <chr.demey@outlook.com>
2018-06-24 11:21:35 +0200

    First commit.

---
+++ BurgersAndFries/BurgersAndFries/BurgersAndFries.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace BurgersAndFries.Droid
+{
+    [Activity(Label = "BurgersAndFries", Icon = "@mipmap/icon", Theme = "@style/MainTheme",  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
